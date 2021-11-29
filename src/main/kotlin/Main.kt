a271f36c24aeb14ddc96fea0da0707e4909006a8
Ricardo <rgallego@iesrafaelalberti.es>
2021-11-29 20:57:08 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+class Pila<T>()
+{
+    var list = mutableListOf<T>()
+
+    fun top(): T {
+        return list.first()
+    }
+    fun push(something:T){
+        list.add(0,something)
+    }
+    fun pop(){
+        list.removeAt(0)
+    }
+    fun empty(): Boolean{
+        return list.isEmpty()
+    }
+
+    fun reverse(list: List<T>):List<T>{
+        val pila = Pila<T>()
+        val sequence1 = list<T> { list.iterator() }
+    }
+}
+
+
+
+fun main() {
+    /*
+    var numbers = listOf("one", "two", "three", "four")
+    var numbersRev = reverse(numbers)
+    if (!listOf("four", "three", "two", "one").equals(numbersRev))
+        println("Error")
+    else
+        println("Correcto")
+    println(numbersRev)
+
+     */
 }
